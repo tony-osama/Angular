ee5243ffae58e5bf7df805bae23ef311dbb72401
tony-osama <tonyosama1546@gmail.com>
2025-03-14 09:31:29 +0200

    add home and info components with routing and navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'App';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './info/info.component';
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'info', component: InfoComponent },
+];
